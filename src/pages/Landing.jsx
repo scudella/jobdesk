ac9a13c9f9abb41d9ea587ea24e5f517d90ec0dc
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-02 12:09:55 -0300

    Landing Page and Logo component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,35 @@
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby vinyl four dollar toast kale chips, godard PBR&B 3 wolf
+            moon knausgaard deep v. Meh tousled hashtag, chartreuse chambray
+            bruh iceland squid kitsch enamel pin blackbird spyplane.
+          </p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
 };
+
 export default Landing;
