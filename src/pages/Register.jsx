ac7c9b95c5ccd54b4ff4e1c4d90e2af4fc2261db
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-03 10:42:57 -0300

    Register Page and FormRow Component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' defaultValue='edu' />
+        <FormRow
+          type='text'
+          name='lastname'
+          labelText='last name'
+          defaultValue='liba'
+        />
+        <FormRow type='text' name='location' defaultValue='cps' />
+        <FormRow type='email' name='email' defaultValue='edu@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='secreto456' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
