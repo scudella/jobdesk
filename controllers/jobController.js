e721341d2ee4c10cb191d0df5cf8cd4925dfed00
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-09 11:35:46 -0300

    Change all Job controllers to use db

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,7 @@
 import Job from '../models/JobModel.js';
-import { nanoid } from 'nanoid';
-
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end' },
-  { id: nanoid(), company: 'google', position: 'back-end' },
-];
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
@@ -18,7 +13,9 @@ export const createJob = async (req, res) => {
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+
+  const job = await Job.findById(id);
+
   if (!job) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
@@ -27,29 +24,28 @@ export const getJob = async (req, res) => {
 
 export const updateJob = async (req, res) => {
   const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
 
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+
+  const updatedJob = await Job.findByIdAndUpdate(
+    id,
+    { company, position },
+    { new: true }
+  );
+  if (!updatedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  job.company = company;
-  job.position = position;
-  console.log(jobs);
-  res.status(200).json({ msg: 'job modified', job });
+  res.status(200).json({ msg: 'job modified', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+
+  const removedjob = await Job.findByIdAndDelete(id);
+
+  if (!removedjob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-  res.status(200).json({ msg: 'job deleted' });
+  res.status(200).json({ msg: 'job deleted', job: removedjob });
 };
