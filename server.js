b7e0187e1277f1481445f74ee2fa23e7dc22ce91
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-09 09:45:32 -0300

    MongoDB Atlas connection and JobModel Schema

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 //routers
 import jobRouter from './routes/jobRouter.js';
@@ -37,6 +38,12 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5100;
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}...`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on port ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
