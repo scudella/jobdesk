a1092ef39826a6a49d111d82bdf1e2c7969551c9
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-14 12:12:19 -0300

    Hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,24 @@
 import User from '../models/UserModel.js';
 import { StatusCodes } from 'http-status-codes';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const { name, email, password, lastName, location } = req.body;
+
   const isFirstAccount = (await User.countDocuments()) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
+
+  const hashedPassword = await hashPassword(password);
+
   const user = await User.create({
     name,
     email,
-    password,
+    password: hashedPassword,
     lastName,
     location,
     role,
   });
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 
 export const login = async (req, res) => {
