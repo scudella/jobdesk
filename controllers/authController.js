5fbdf4575216718553036c0ecac5d4b2519782e4
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-14 11:43:16 -0300

    Register validation and user creation

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,16 @@ import { StatusCodes } from 'http-status-codes';
 
 export const register = async (req, res) => {
   const { name, email, password, lastName, location } = req.body;
-  const user = await User.create({ name, email, password, lastName, location });
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+  const user = await User.create({
+    name,
+    email,
+    password,
+    lastName,
+    location,
+    role,
+  });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
