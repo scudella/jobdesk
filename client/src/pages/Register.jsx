9f2a93b402a03fd9f13cb22175e01125ff89fed8
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-16 10:55:54 -0300

    Register Page Complete
    action, redirect, useNavigation, formData, and Form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,25 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/register', data);
+    toast.success('Registration Successful');
+    return redirect('/login'); // Only use redirect in action
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   return (
     <Wrapper>
-      <form className='form'>
+      <Form method='post' className='form'>
         <Logo />
         <h4>Register</h4>
         <FormRow type='text' name='name' defaultValue='edu' />
         <FormRow
           type='text'
-          name='lastname'
+          name='lastName'
           labelText='last name'
           defaultValue='liba'
         />
         <FormRow type='text' name='location' defaultValue='cps' />
         <FormRow type='email' name='email' defaultValue='edu@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secreto456' />
-        <button type='submit' className='btn btn-block'>
-          submit
+        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>
           Already a member?
@@ -27,7 +44,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
