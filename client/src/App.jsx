3938f6ab08ac2206c827dc899d4bf11f2431bfde
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-17 17:32:02 -0300

    Admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ import { action as deleteJobAction } from './pages/DeleteJob';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { loader as allJobsLoader } from './pages/AllJobs';
 import { loader as editJobLoader } from './pages/EditJob';
+import { loader as adminLoader } from './pages/Admin';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -78,6 +79,7 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: 'edit-job/:id',
