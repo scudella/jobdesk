f9929a943a67e6f50c583d16245bac35d5758fbd
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-23 09:23:43 -0300

    frontend for stats page
    StatsContainer, ChartsContainer, AreaChart and BarChart components
    Add recharts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,7 @@ import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { loader as allJobsLoader } from './pages/AllJobs';
 import { loader as editJobLoader } from './pages/EditJob';
 import { loader as adminLoader } from './pages/Admin';
+import { loader as statsLoader } from './pages/Stats';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -67,6 +68,7 @@ const router = createBrowserRouter([
           {
             path: 'stats',
             element: <Stats />,
+            loader: statsLoader,
           },
           {
             path: 'all-jobs',
