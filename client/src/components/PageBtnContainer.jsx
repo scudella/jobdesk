a4e10972f37341a2d4e9580cf5d1719863a5e299
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-24 12:11:03 -0300

    pagination displaying dots to reduce page buttons displayed

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -21,6 +21,82 @@ const PageBtnContainer = () => {
     navigate(`${pathname}?${searchparams.toString()}`);
   };
 
+  const addPageButton = ({ pageNumber, activeClass }) => {
+    return (
+      <button
+        className={`btn page-btn ${activeClass && 'active'}`}
+        key={pageNumber}
+        onClick={() => handlePageChange(pageNumber)}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+
+  const renderPageButtons = () => {
+    const pageButtons = [];
+
+    // first page
+    pageButtons.push(
+      addPageButton({ pageNumber: 1, activeClass: currentPage === 1 })
+    );
+
+    // dots button
+    if (currentPage > 3) {
+      pageButtons.push(
+        <span className='page-btn dots' key='dots-1'>
+          ...
+        </span>
+      );
+    }
+
+    // the page before current page
+    if (currentPage !== 1 && currentPage !== 2) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: currentPage - 1,
+          activeClass: false,
+        })
+      );
+    }
+    // current page
+    if (currentPage !== 1 && currentPage !== numOfPages) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: currentPage,
+          activeClass: true,
+        })
+      );
+    }
+    // the page after current page
+    if (currentPage !== numOfPages && currentPage !== numOfPages - 1) {
+      pageButtons.push(
+        addPageButton({
+          pageNumber: currentPage + 1,
+          activeClass: false,
+        })
+      );
+    }
+
+    // dots button
+    if (currentPage < numOfPages - 2) {
+      pageButtons.push(
+        <span className='page-btn dots' key='dots+1'>
+          ...
+        </span>
+      );
+    }
+
+    // last page
+    pageButtons.push(
+      addPageButton({
+        pageNumber: numOfPages,
+        activeClass: currentPage === numOfPages,
+      })
+    );
+    return pageButtons;
+  };
+
   return (
     <Wrapper>
       <button
@@ -36,21 +112,7 @@ const PageBtnContainer = () => {
         <HiChevronDoubleLeft />
         prev
       </button>
-      <div className='btn-container'>
-        {pages.map((pageNumber) => {
-          return (
-            <button
-              className={`btn page-btn ${
-                pageNumber === currentPage && 'active'
-              }`}
-              key={pageNumber}
-              onClick={() => handlePageChange(pageNumber)}
-            >
-              {pageNumber}
-            </button>
-          );
-        })}
-      </div>
+      <div className='btn-container'>{renderPageButtons()}</div>
       <button
         className='btn next-btn'
         onClick={() => {
