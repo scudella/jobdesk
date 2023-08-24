5abde0a17ca91f95024b803b82416d709c208c5f
Eduardo S. Libardi <eslibardi@gmail.com>
2023-08-24 09:58:36 -0300

    Pagination, initial setup

---
+++ client/src/components/PageBtnContainer.jsx
@@ -0,0 +1,41 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useAllJobsContext } from '../pages/AllJobs';
+
+const PageBtnContainer = () => {
+  const {
+    data: { numOfPages, currentPage },
+  } = useAllJobsContext();
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  return (
+    <Wrapper>
+      <button className='btn prev-btn'>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              className={`btn page-btn ${
+                pageNumber === currentPage && 'active'
+              }`}
+              key={pageNumber}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className='btn next-btn'>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+export default PageBtnContainer;
